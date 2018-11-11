13ebf84235c85fd3cbbf16dca0e0c4a144bbc284
minaamfasihi <minaam.fasihi@gmail.com>
2018-11-12 03:36:49 +0500

    changes

--- wp-content/themes/advanced-theme/functions.php
+++ wp-content/themes/advanced-theme/functions.php
@@ -1,5 +1,8 @@
 <?php
     function adv_theme_support() {
+      // Featured Image support
+      add_theme_support('post-thumbnails');
+
       // Nav Menus
       register_nav_menus(array(
         'primary'   => __('Primary Menu'),
--- wp-content/themes/advanced-theme/single.php
+++ wp-content/themes/advanced-theme/single.php
@@ -29,9 +29,8 @@
                   <?php the_post_thumbnail(); ?>
                 </div>
               <?php endif; ?>
-              <?php the_excerpt(); ?>
+              <?php the_content(); ?>
             </p>
-    				<a class="button" href="<?php the_permalink(); ?>">Read More</a>
     			</article>
         <?php endwhile; ?>
       <?php else : ?>
